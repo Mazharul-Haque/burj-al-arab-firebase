7519e63015d1f848bc47914ecfc715db4637c9e4
Mazharul-Haque <mshagor009@gmail.com>
2021-03-18 17:59:51 +0600

    add usestate

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,39 @@
 import React, { useContext } from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config'
+
 
 
 const Login = () => {
+    const [loggedInUser, setLoggedInUser] = useContext();
+    firebase.initializeApp(firebaseConfig);
+
+    const handleGoogleSignIn = () => {
+        var provider = new firebase.auth.GoogleAuthProvider();
+
+        firebase.auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                const {displayName, email} = result.user;
+                const signedInUser = {name : displayName, email}
+                console.log(signedInUser);
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+            });
+    }
     return (
         <div>
             <h1>This is Login</h1>
+            <h2 onClick={handleGoogleSignIn} className="btn btn-success" >Google Sign-In</h2>
         </div>
     );
 };
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCRbPJ3FDmiVS5-CFV3-asckBMFrUTJYQs",
+    authDomain: "burj-al-arab-29de6.firebaseapp.com",
+    projectId: "burj-al-arab-29de6",
+    storageBucket: "burj-al-arab-29de6.appspot.com",
+    messagingSenderId: "772941539910",
+    appId: "1:772941539910:web:f38e46c5b56e9b5a94b847"
+  };
+
+  export default firebaseConfig;
